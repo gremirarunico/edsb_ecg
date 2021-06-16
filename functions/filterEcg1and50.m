7411ac2b2a2d124e8a4ec97d3c6e71b3a04c2646
gremirarunico <marco.guerrini@outlook.com>
2021-06-16 20:04:01 +0200

    Corretta risposta all'impulso filtro iir

--- functions/filterEcg1and50.m
+++ functions/filterEcg1and50.m
@@ -4,6 +4,11 @@ function sigout = filterEcg1and50(sigin, sf)
     'HalfPowerFrequency1',1,'HalfPowerFrequency2',50,...
     'SampleRate',sf);
 
-    sigout = filtfilt(bpFilt, sigin);
+    %sigout = filtfilt(bpFilt, sigin);
+    % aggiunta per eliminare risposta all'impulso
+    ignoreSize = 10000; % numero di zeri all'inizio e alla fine
+    tempSig = [zeros(ignoreSize, 1); sigin(:); zeros(ignoreSize, 1)];
+    filtredTempSig = filtfilt(bpFilt, tempSig);
+    sigout = filtredTempSig(ignoreSize+1:end-ignoreSize);
 end
 
