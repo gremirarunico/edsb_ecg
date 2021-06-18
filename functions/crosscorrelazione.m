6d13c7094647fe6df62e18633ef2d55cfdd9a858
Nenyuki <ireneboccato@gmail.com>
2021-06-18 11:22:06 +0200

    crosscorrelazione

--- functions/crosscorrelazione.m
+++ functions/crosscorrelazione.m
@@ -1,28 +1,29 @@
+% crosscorrelazione.m
+% Metrica di similitudine (cross-correlazione normalizzata) per la
+% rilevazione delle forme d'onda 
+
 function [annotations, GammaSigma] = crosscorrelazione(signal, template, threshold)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+
 %k=costante relativa al ritardo
 lSignal = length(signal);
-D = length(template);
-templateMean = mean(template);
+D = length(template); 
+templateMean = mean(template); %valor medio del template
 
+% in ogni finestra di D campioni si calcola la media del segnale
 for k = 1:(lSignal - D + 1)
-    signalMean = mean(signal(k:k+D-1));
+    signalMean = mean(signal(k:k-1+D));
     
     for i=1:D
         Numeratore(i)=(template(i)-templateMean)*(signal(i+k-1)-signalMean);
-        %Denominatore_fatt1(i)= (template(i)-mean(template))^2;
-        %Denominatore_fatt2(i)= ((signal(i+k-1)-mean(signal(k:k+D-1))))^2;
     end
     NumeratoreTot=sum(Numeratore)/(D-1);
-    %Denominatore_fatt1_tot=sum(Denominatore_fatt1);
-    %Denominatore_fatt2_tot=sum(Denominatore_fatt2);
-    %DenominatoreTot=Denominatore_fatt1_tot*Denominatore_fatt2_tot;
-    %Denominatore=sqrt(DenominatoreTot);
+    
+    % la std del segnale è calcolata in ogni finestra di D campioni
     DenominatoreTot = std(template) * std(signal(k:k+D-1));
     GammaSigma(k)=NumeratoreTot/DenominatoreTot;
 end
 
+% zero padding prima e dopo per allineamento con il segnale  
 GammaSigma = [zeros(1, (D-1)/2), GammaSigma, zeros(1, (D-1)/2)];
 
 % plot(GammaSigma);
