11b98b147931d2fd01a9ef8b4214941b7172e4e5
gremirarunico <marco.guerrini@outlook.com>
2021-06-18 10:43:04 +0200

    update

--- fraden_track_00_filter_instability.m
+++ fraden_track_00_filter_instability.m
@@ -1,20 +1,20 @@
 % disegna l'inizio dei file per la traccia 00 mostrando il risultato post
-% filtraggio con e senza la rimozione del segnale di calibrazione per l'ECG
-% mostrando la risposta all'impulso del filtro che va a causare problemi
-% all'algoritmo fraden newman per via delle ampie oscillazioni iniziali
+% filtraggio con e senza zero padding del segnale ECG mostrando la risposta
+% all'impulso del filtro che va a causare problemi all'algoritmo fraden
+% newman per via delle ampie oscillazioni iniziali
 
 addpath './functions'
 
 [points, attributes] = loadphysionet('ecg', '00');
 [gold, extras] = loadphysionet('atr', '00');
-filtredSig = filterEcg1and50(points(:,1), attributes.samplingFrequency);
-cleanedSig = removeCalibrationSignal(points(:,1));
-filtredSig2 = filterEcg1and50(cleanedSig, attributes.samplingFrequency);
+filtredSig = filterEcg1and50instable(points(:,1), attributes.samplingFrequency);
+
+filtredSig2 = filterEcg1and50(points(:,1), attributes.samplingFrequency);
 figure('Name','Traccia 00 filtrata con e senza rimozione calibrazione','NumberTitle','off');
 hold on
 
 plot(filtredSig);
 plot(filtredSig2);
-title("Traccia 00 filtrata con e senza rimozione calibrazione");
-legend('Con segnale di calibrazione','Senza segnale di calibrazione');
+title("Traccia 00 filtrata con e senza zero padding");
+legend('Senza zero padding','Con zero padding');
 xlim([1, 1000]);
