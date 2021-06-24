c8821ec307d9269254d195d926226b8606152b94
gremirarunico <marco.guerrini@outlook.com>
2021-06-24 15:32:15 +0200

    u

--- functions/predictiveValue.m
+++ functions/predictiveValue.m
@@ -1,5 +1,14 @@
-function [pos,neg] = predictiveValue(TP,FP,TN,FN)
-    pos = TP./(TP+FP);
-    neg = TN./(TN+FN);
+function [VPP,VPN] = VP(FN, FP, TP, TN)
+VPP = TP./(TP+FP);
+VPN = TN./(TN+FN);
+
+if sum(isnan(VPP))
+    VPP(find(isnan(VPP))) = 1;
+end
+
+if sum(isnan(VPN))
+    VPN(find(isnan(VPN))) = 1;
+end
+
 end
 
